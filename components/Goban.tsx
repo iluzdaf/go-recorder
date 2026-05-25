a007bd6282b1fa9fcf34ebb8bb6dab0a21b39382
iluzdaf <fadzuli.said@gmail..com>
2026-05-25 10:38:16 +0800

    Wrap Shudan Goban for React type compatibility

--- components/Goban.tsx
+++ components/Goban.tsx
@@ -1,6 +1,9 @@
 "use client";
 
-import { Goban } from "@sabaki/shudan";
+import { Goban as ShudanGoban } from "@sabaki/shudan";
+import type { ComponentType } from "react";
+
+const Goban = ShudanGoban as unknown as ComponentType<any>;
 
 export default function GoBoard() {
     const size = 19;
@@ -11,10 +14,7 @@ export default function GoBoard() {
 
     return (
         <div className="p-4">
-            <Goban
-                vertexSize={24}
-                signMap={signMap}
-            />
+            <Goban vertexSize={24} signMap={signMap} />
         </div>
     );
 }
